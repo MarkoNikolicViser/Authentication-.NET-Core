aadff900dd00c32f981f654d0691cac9ee2865d2
Marko <59150246+MarkoNikolicViser@users.noreply.github.com>
2020-04-16 20:48:42 +0200

    initial commit

---
+++ Autentikacija/Areas/Identity/Data/AutentikacijaDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autentikacija.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autentikacija.Data
+{
+    public class AutentikacijaDbContext : IdentityDbContext<AutentikacijaUser>
+    {
+        public AutentikacijaDbContext(DbContextOptions<AutentikacijaDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Autentikacija/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Autentikacija.Areas.Identity.Data;
+using Autentikacija.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Autentikacija.Areas.Identity.IdentityHostingStartup))]
+namespace Autentikacija.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AutentikacijaDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AutentikacijaDbContextConnection")));
+
+                services.AddDefaultIdentity<AutentikacijaUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<AutentikacijaDbContext>();
+            });
+        }
+    }
+}
